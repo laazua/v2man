9e9ffaff5a6b8282469930296599c8b80f6a4f23
laazua <laazua@github.com>
2026-06-22 17:29:48 +0800

    ..

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/nodes/', include('nodes.urls')),
     path('api/subscription/', include('nodes.urls_sub')),
