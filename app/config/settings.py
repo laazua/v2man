9e2f209853c7522cc80bbaf1ce17b951ba00e4c2
laazua <laazua@github.com>
2026-06-23 16:38:07 +0800

    feat: 更新限流配置，移除注册限流，新增验证码限流

--- app/config/settings.py
+++ app/config/settings.py
@@ -212,8 +212,8 @@ REST_FRAMEWORK = {
         'anon': '20/hour',
         'user': '1000/hour',
         'login': '10/minute',
-        'register': '3/hour',
         'password_reset': '5/hour',
+        'verification_code': '1/minute',
     },
 }
 
